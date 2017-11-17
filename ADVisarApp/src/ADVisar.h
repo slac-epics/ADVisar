2208769475fd4df7a5e6ac81cbb46085f2db1632
joaoprod <joaoprod@slac.stanford.edu>
2017-11-17 14:29:28 -0800

    2

---
+++ ADVisarApp/src/ADVisar.h
@@ -0,0 +1,29 @@
+#ifndef	ADVisar_H
+#define ADVisar_H
+
+/*
+ *	Author:  joaoprod
+ *
+ *	Abstract:
+ *		TODO:	What does this lib provide?
+ */
+
+#ifdef	__cplusplus
+extern "C" {
+#endif	/*	__cplusplus	*/
+
+
+/*
+ * Constants
+ */
+
+
+/*
+ * Function declarations
+ */
+int		ADVisarInit( );
+
+#ifdef	__cplusplus
+}
+#endif	/*	__cplusplus	*/
+#endif	/*	ADVisarH	*/
